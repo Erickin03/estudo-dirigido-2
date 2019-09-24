0826668709fdd4209834a24b835feb2050336306
Erickin03 <55162052+Erickin03@users.noreply.github.com>
2019-09-25 03:53:42 +0900

    Create teste.cpp

---
+++ teste.cpp
@@ -0,0 +1,70 @@
+Algotitimos de Repetição 
+
+1) 369 
+2) 12345
+
+Listas
+
+1) 12345
+2) 99
+3)CuritibaMedianeiraFlorianopolisJoinvile
+
+Pratica 
+
+1) 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void NUMEROS (int i) {
+
+do {
+
+cout << i << "\n";
+
+i++;
+
+} while (i <= 100);
+
+}
+
+int main(){
+
+NUMEROS (1);
+
+return 0;
+}
+
+2)
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int a, b;
+
+int SOMA (int a, int b) {
+
+int res;
+
+res = a + b;
+
+cout << res;
+
+}
+
+int main(){
+
+cin >> a;
+cin >> b;
+
+SOMA (a, b);
+
+
+return 0;
+}
+
+
